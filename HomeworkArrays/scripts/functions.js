2cacbdd20e1399942a7b7d8e4cd94d576fe71770
plamengeorgiev <plamen.flame@gmail.com>
2014-06-18 12:21:05 +0300

    All my homeworks from JavaScript Basics
    All my homeworks from JavaScript Basics

---
+++ HomeworkArrays/scripts/functions.js
@@ -0,0 +1,38 @@
+﻿function PrintArray(array) {
+    for (var i = 0, len = array.length; i < len; i++) {
+        jsConsole.writeLine('index: ' + i + ' => element: ' + array[i]);
+    }
+}
+
+function selectionSort(array) {
+    var min = array[0];
+    var temp = 0;
+    for (var i = 0, len = array.length ; i < len; i++) {
+        for (var j = i; j < len; j++) {
+            if (parseInt(array[j]) < parseInt(array[i])) {
+                temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+    }
+    return array;
+}
+
+function binarySearch(array, key, min, max) {
+    if (parseInt(max) < parseInt(min)) {
+        return -1;
+    }
+    else {
+        var mid = Math.floor((min + max) / 2);
+        if (parseInt(array[mid]) > parseInt(key)) {
+            return binarySearch(array, key, min, mid - 1);
+        }
+        else if (parseInt(array[mid]) < parseInt(key)) {
+            return binarySearch(array, key, mid + 1, max);
+        }
+        else {
+            return mid;
+        }
+    }
+}
